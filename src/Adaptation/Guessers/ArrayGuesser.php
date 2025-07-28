8b0584df5648b66006ffc06ea426847587392feb
Пётр Шкумат <p.shkumat@webpractik.ru>
2025-07-28 17:56:01 +0300

    feat(dto): убрана генерация классов массивов
    Refs: #35

--- src/Adaptation/Guessers/ArrayGuesser.php
+++ src/Adaptation/Guessers/ArrayGuesser.php
@@ -3,7 +3,6 @@
 namespace Webpractik\Bitrixapigen\Adaptation\Guessers;
 
 use Jane\Component\JsonSchema\Generator\Naming;
-use Jane\Component\JsonSchema\Guesser\Guess\ClassGuess;
 use Jane\Component\JsonSchema\Registry\Registry;
 use Jane\Component\JsonSchemaRuntime\Reference;
 use Jane\Component\OpenApiCommon\Guesser\OpenApiSchema\ArrayGuesser as JaneArrayGuesser;
@@ -24,22 +23,9 @@ class ArrayGuesser extends JaneArrayGuesser
      */
     public function guessClass($object, string $name, string $reference, Registry $registry): void
     {
-        if (!$registry->hasClass($reference)) {
-            $schema = $registry->getSchema($reference);
-            if (null !== $schema) {
-                $classGuess = $this->createClassGuess($object, $reference, $name);
-                $schema->addClass($reference, $classGuess);
-            }
-        }
-
         $items = $object->getItems();
         if ($items instanceof Reference || is_a($items, $this->getSchemaClass())) {
             $this->chainGuesser->guessClass($items, $name . 'Item', $reference . '/items', $registry);
         }
-    }
-
-    private function createClassGuess($object, string $reference, string $name): ClassGuess
-    {
-        return new ClassGuess($object, $reference, $this->naming->getClassName($name), [], $object->getDeprecated());
     }
 }
