0a3cc51e4892e6fdaa7cfdfbfc47c234fcb14033
Пётр Шкумат <p.shkumat@webpractik.ru>
2025-07-29 17:56:53 +0300

    feat(dto): изменено вычисление значений по умолчанию
    Refs: #35

--- src/Files/Settings/MethodParameterSettings.php
+++ src/Files/Settings/MethodParameterSettings.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace Webpractik\Bitrixapigen\Files\Settings;
 
 use LogicException;
-use PhpParser\BuilderHelpers;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\NullableType;
+use PhpParser\Node\Scalar;
+use UnitEnum;
+
+use function get_class;
 
 class MethodParameterSettings
 {
@@ -34,6 +40,48 @@ class MethodParameterSettings
             return null;
         }
 
-        return BuilderHelpers::normalizeValue($this->default);
+        return $this->normalizeValue($this->default);
+    }
+
+    private function normalizeValue($value): ?Expr
+    {
+        if ($value instanceof Expr) {
+            return $value;
+        }
+
+        if (is_null($value)) {
+            return new Expr\ConstFetch(
+                new Name('null')
+            );
+        }
+
+        if (is_bool($value)) {
+            return new Expr\ConstFetch(
+                new Name($value ? 'true' : 'false')
+            );
+        }
+
+        if (is_int($value)) {
+            return new Scalar\Int_($value);
+        }
+
+        if (is_float($value)) {
+            return new Scalar\Float_($value);
+        }
+
+        if (is_string($value)) {
+            return new Scalar\String_($value);
+        }
+
+        // TODO
+        if (is_array($value)) {
+            return null;
+        }
+
+        if ($value instanceof UnitEnum) {
+            return new Expr\ClassConstFetch(new FullyQualified(get_class($value)), new Identifier($value->name));
+        }
+
+        throw new LogicException('Invalid value');
     }
 }
